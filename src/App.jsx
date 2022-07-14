9be24870df64acdeec7a067ae07141c0d5643fe0
Avan <layouwen@gmail.com>
2022-07-14 11:40:27 +0800

    feat: 使用 redux

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { Component } from 'react'
+import store from './store'
+
+class App extends Component {
+  componentDidMount () {
+    store.subscribe(() => {
+      this.forceUpdate()
+    })
+  }
+
+  onAdd = () => {
+    store.dispatch({ type: 'ADD' })
+  }
+
+  onSub = () => {
+    store.dispatch({ type: 'SUB' })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <div>
+          <div>value: {store.getState()}</div>
+          <button onClick={this.onAdd}>add</button>
+          <button onClick={this.onSub}>sub</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1;
+    case 'SUB':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const reduxStore = createStore(countReducer);
+
+export default reduxStore;
