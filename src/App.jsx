2074b2f2e63c10e50df650c7d5decc08e087579d
Avan <layouwen@gmail.com>
2022-07-15 12:48:31 +0800

    feat: add sync dispatch demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import './App.css'
-import { Component } from 'react'
-import store from './store'
-import './demo/compose.js'
+import "./App.css";
+import { Component } from "react";
+import store from "./store";
+import "./demo/compose.js";
 
 class App extends Component {
-  componentDidMount () {
+  componentDidMount() {
     store.subscribe(() => {
-      this.forceUpdate()
-    })
+      this.forceUpdate();
+    });
   }
 
   onAdd = () => {
-    store.dispatch({ type: 'ADD' })
-  }
+    store.dispatch({ type: "ADD" });
+  };
 
   onSub = () => {
-    store.dispatch({ type: 'SUB' })
-  }
+    store.dispatch({ type: "SUB" });
+  };
 
-  render () {
+  onSyncAdd = () => {
+    store.dispatch((dispatch) => {
+      setTimeout(() => {
+        dispatch({ type: "ADD" });
+      }, 1000);
+    });
+  };
+
+  render() {
     return (
       <div className="App">
         <div>
           <div>value: {store.getState()}</div>
           <button onClick={this.onAdd}>add</button>
           <button onClick={this.onSub}>sub</button>
+          <button onClick={this.onSyncAdd}>syncAdd</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
